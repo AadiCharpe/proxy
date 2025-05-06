601ae0d4b834d309868fe0e2d63d0b55a2e4642f
aadic <aadi.charpe@hotmail.com>
2025-05-05 19:54:29 -0700

    replaces <a> tags

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, request
+from bs4 import BeautifulSoup
 import requests
 
 app = Flask(__name__)
 
 @app.route('/proxy')
 def get_stuff():
-    return requests.get(request.args.get('url')).content
+    soup = BeautifulSoup(requests.get(request.args.get('url')).content, 'html.parser')
+    for a in soup.find_all('a'):
+        a['href'] = f"{request.base_url}?url={a['href']}"
+    return str(soup)
 
 app.run()
