f762f1daf1215be93b10ab62df51aaf3e8175188
aadic <aadi.charpe@hotmail.com>
2025-05-06 18:48:35 -0700

    handles more urls

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from flask import Flask, request
 from bs4 import BeautifulSoup
-import requests
+from urllib.parse import urlparse
+from requests import get
 
 app = Flask(__name__)
 
 @app.route('/proxy')
 def get_stuff():
-    soup = BeautifulSoup(requests.get(request.args.get('url')).content, 'html.parser')
+    soup = BeautifulSoup(get(request.args.get('url')).content, 'html.parser')
     for a in soup.find_all('a'):
-        a['href'] = f"{request.base_url}?url={a['href']}"
+        href = a['href']
+        if href.startswith('/'):
+            href = urlparse(request.args.get('url')).netloc + href
+        if not href.startswith('http'):
+            href = 'https://' + href
+        a['href'] = f"{request.base_url}?url={href}"
     return str(soup)
 
 app.run()
